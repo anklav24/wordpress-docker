05e7a297a9f00fdcd937cbf73aea4c5bc8ce91ae
Aleksandr Lobanov <anklav24@gmail.com>
2021-12-17 18:40:24 +0700

    Added: Files rotation 4soulsband_wordpress_backup.sh

--- 4soulsband_wordpress_backup.sh
+++ 4soulsband_wordpress_backup.sh
@@ -11,27 +11,32 @@ then
 fi
 
 out_file="$task_name".tar.gz
-logfile_name="$task_name"_backup.log
 
 source_dir=4soulsband_wordpress
 source_db=4soulsband_mysql_db
 source_config_dir=deploy_configs
 
 backup_path=./4soulsband_backup
+
+logfile_name="$task_name"_backup.log
 logfile_path=$backup_path/$logfile_name
 
 mkdir --parents $backup_path
 
-status="Status: "$?
-echo $status >> $logfile_path
+#status="Status: "$?
+#echo $status >> $logfile_path
 
-# Rotate backups
-#find $backup_path/ -mtime +$days_to_keep -delete
-find $backup_path/ -mtime +$days_to_keep |& tee -a $logfile_path
 
 # Log rotate
 tail -n 10 $logfile_path > /tmp/"$logfile_name".tmp
 mv -f /tmp/"$logfile_name".tmp $logfile_path
+
+# Rotate backups
+#find $backup_path/ -mtime +$days_to_keep -delete
+echo "Delete files older than "$days_to_keep" (Day/Days)" |& tee -a $logfile_path
+find $backup_path/* ! -path $logfile_path -mtime $days_to_keep |& tee -a $logfile_path
+find $backup_path/* ! -path $logfile_path -mtime $days_to_keep -delete |& tee -a $logfile_path
+echo "Deletion completed" |& tee -a $logfile_path
 
 # Show log
 cat $logfile_path
